a164ffed6163da2b6f89446b329cc36c07d3cbee
HelenBassa <helenbassa@gmail.com>
2021-09-06 23:36:00 +0300

    fix: moved similar data to the objects

--- client/src/components/footer/footer.tsx
+++ client/src/components/footer/footer.tsx
@@ -1,53 +1,48 @@
 import React from 'react';
 import '@styles/footer.sass';
 
-export const Footer: React.FC = () => (
-  <footer className="footer">
-    <div className="container footer-wrapper">
-      <div className="footer-github">
-        <ul className="github-list">
-          <li>
-            <a
-              href="https://github.com/utrennik"
-              className="github-item"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              utrennik
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/RealOneReal"
-              className="github-item"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              RealOneReal
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/HelenBassa"
-              className="github-item"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              HelenBassa
-            </a>
-          </li>
-        </ul>
+export const Footer: React.FC = () => {
+  const team = [
+    {
+      name: 'utrennik',
+      github: 'https://github.com/utrennik',
+    },
+    {
+      name: 'RealOneReal',
+      github: 'https://github.com/RealOneReal',
+    },
+    {
+      name: 'HelenBassa',
+      github: 'https://github.com/HelenBassa',
+    },
+  ];
+
+  const data = {
+    style: 'github-item',
+    target: '_blank',
+    rel: 'noopener noreferrer',
+  };
+
+  return (
+    <footer className="footer">
+      <div className="container footer-wrapper">
+        <div className="footer-github">
+          <ul className="github-list">
+            {team.map((item) => (
+              <li>
+                <a href={item.github} className={data.style} target={data.target} rel={data.rel}>
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="footer-rsschool">
+          <a href="https://rs.school/react/" className="rss" target={data.target} rel={data.rel}>
+            <span className="rss-year">'21</span>
+          </a>
+        </div>
       </div>
-      <div className="footer-rsschool">
-        <a
-          href="https://rs.school/react/"
-          className="rss"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="rss-year">'21</span>
-        </a>
-      </div>
-    </div>
-  </footer>
-);
+    </footer>
+  );
+};
