9b99339160ba2a0f744a009e583c2c056dadec0b
Utrennik <utrennik@list.ru>
2021-09-06 07:20:15 +0300

    feat: integrate material ui design system

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import '@styles/main.sass';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
 
 const App: React.FC = () => (
   <div className="_container">
-    <h3>Hello, pointing poker!</h3>
+    <h3>
+      Hello, pointing poker!
+      <Icon color="primary">star</Icon>
+    </h3>
+    <Button variant="contained">Hello!</Button>
   </div>
 );
 
