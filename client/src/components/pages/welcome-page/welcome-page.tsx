47e531832d90f646a54fde7cbe3d4e21ed994345
Utrennik <utrennik@list.ru>
2021-09-06 21:15:17 +0300

    init: initial commit

---
+++ client/src/components/pages/welcome-page/welcome-page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import './welcome-page.sass';
+
+const WelcomePage = () => <div className="_container">Welcome page</div>;
+
+export default WelcomePage;
