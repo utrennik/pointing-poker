8822aa62dafa0d0670c9faa403f357f173603003
HelenBassa <helenbassa@gmail.com>
2021-09-13 10:30:04 +0300

    fix: delete test navbar

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -1,22 +1,11 @@
 import { NavLink } from 'react-router-dom';
 
-import routes from '@components/router/routes';
-
 import '@styles/header.sass';
 
 const Header = () => (
   <header className="header">
     <div className="header-up">
       <div className="container header-up-wrapper">
-        <ul className="nav-wrapper">
-          {routes.map(({ title, path }) => (
-            <li key={title}>
-              <NavLink exact activeClassName="active" className="nav-link" to={path}>
-                {title}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
         <div className="chat-wrapper">
           <div className="chat-icon"></div>
         </div>
