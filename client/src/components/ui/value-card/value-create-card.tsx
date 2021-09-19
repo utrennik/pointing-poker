9016c32fb5ea9ae12c58d71f03614d0526b1578f
HelenBassa <helenbassa@gmail.com>
2021-09-20 00:07:58 +0300

    feat: add Value card without set value

--- client/src/components/ui/value-card/value-create-card.tsx
+++ client/src/components/ui/value-card/value-create-card.tsx
@@ -1,14 +1,16 @@
 import { Card, IconButton } from '@material-ui/core';
+import { valueCardData } from '@components/ui/value-card/value-cardData';
 import './value-card.sass';
 
 export const ValueCreateCard = () => {
-  const handleAddCoverCard = (event: React.MouseEvent<HTMLButtonElement>) => {
-    console.log(event.target);
+  const handleAddValueCard = () => {
+    valueCardData.push({ name: '', value: '' });
+    console.log('renderNewCoverCard', valueCardData);
   };
 
   return (
     <Card className="value-card">
-      <IconButton className="value-card-add-btn" onClick={handleAddCoverCard} />
+      <IconButton className="value-card-add-btn" onClick={handleAddValueCard} />
     </Card>
   );
 };
