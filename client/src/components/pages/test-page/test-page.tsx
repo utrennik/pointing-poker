0a33ebf1ac4c25f91efbcfb2ab801d68da7d043f
HelenBassa <helenbassa@gmail.com>
2021-10-01 12:22:00 +0300

    fix: add position sticky for Game voting section, reduced the height of the Chat component

--- client/src/components/pages/test-page/test-page.tsx
+++ client/src/components/pages/test-page/test-page.tsx
@@ -1,10 +1,11 @@
 import { GameVotingSection } from '@components/pages/game-page/game-voting-section/game-voting-section';
 import '@styles/page.sass';
+import '../game-page/game-page.sass';
 
 const TestPage = () => (
   <main className="main">
     <div className="container content-wrapper game-page">
-      <div>Game Top Section</div>
+      <div className="game-top-section">Game Top Section</div>
       <div className="game-round-issues-section">Game Round Issues Section</div>
       <GameVotingSection />
     </div>
